fd4dd67932acf80b8281599b1d098563a58c54c7
Bob Park <hwpark11@kakao.com>
2021-12-20 22:46:58 +0900

    Initialize ch-01 project

---
+++ ch-01-spring-reactive/src/main/java/com/hwpark/ch01springreactive/Ch01SpringReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.hwpark.ch01springreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch01SpringReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch01SpringReactiveApplication.class, args);
+    }
+
+}
---
+++ ch-01-spring-reactive/src/test/java/com/hwpark/ch01springreactive/Ch01SpringReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwpark.ch01springreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch01SpringReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

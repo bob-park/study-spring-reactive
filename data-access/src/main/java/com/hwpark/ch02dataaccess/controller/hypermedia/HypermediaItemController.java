f6a58e4a046e31346440eaf693c5ca44b7726215
Bob Park <hwpark11@kakao.com>
2021-12-28 02:13:51 +0900

    feat: ALPS 예제 추가

--- data-access/src/main/java/com/hwpark/ch02dataaccess/controller/hypermedia/HypermediaItemController.java
+++ data-access/src/main/java/com/hwpark/ch02dataaccess/controller/hypermedia/HypermediaItemController.java
@@ -2,11 +2,18 @@ package com.hwpark.ch02dataaccess.controller.hypermedia;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Links;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mediatype.alps.Alps;
+import org.springframework.hateoas.mediatype.alps.Type;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +24,8 @@ import com.hwpark.ch02dataaccess.repository.ItemRepository;
 
 import reactor.core.publisher.Mono;
 
+import static org.springframework.hateoas.mediatype.alps.Alps.alps;
+import static org.springframework.hateoas.mediatype.alps.Alps.descriptor;
 import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn;
 
@@ -62,6 +71,21 @@ public class HypermediaItemController {
         return Mono.zip(itemRepository.findById(id), selfLink, aggregateLink)
             .map(o -> EntityModel.of(o.getT1(), Links.of(o.getT2(), o.getT3())));
 
+    }
+
+    @GetMapping(path = "items/profile", produces = MediaTypes.ALPS_JSON_VALUE)
+    public Alps profile() {
+        return alps()
+            .descriptor(Collections.singletonList(descriptor()
+                .id(Item.class.getSimpleName() + "-repr")
+                .descriptor(Arrays.stream(Item.class.getDeclaredFields())
+                    .map(field -> descriptor()
+                        .name(field.getName())
+                        .type(Type.SEMANTIC)
+                        .build())
+                    .collect(Collectors.toList()))
+                .build()))
+            .build();
     }
 
 }
